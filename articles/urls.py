2ddb21643ba0e440ba0349ccdc2ce96163ceccb7
Joakim JFKK. Krugstrup <joak@pc-tec.local>
2020-01-30 14:56:42 +0100

    chapter 13 views

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
 ]
